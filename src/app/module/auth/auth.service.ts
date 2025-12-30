cf17b9f65ba215c826507b2d9034a3750c75adb1
safin33221 <safin33221@gmail.com>
2025-12-31 00:13:54 +0600

    implemnet login system

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -1,9 +1,43 @@
-const login = async (payload: { email: string, password: string }) => {
-    console.log(payload);
-}
+import bcrypt from "bcrypt";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import { prisma } from "../../../lib/prisma";
+import envConfig from "../../../config/env.config";
 
+const login = async (payload: { email: string; password: string }) => {
+    // 1️⃣ Find user
+    const user = await prisma.user.findUnique({
+        where: { email: payload.email },
+    });
 
+    if (!user) {
+        throw new Error("Invalid email or Password");
+    }
+
+    // 2️⃣ Check account status
+    if (!user.isActive) {
+        throw new Error("Account is disabled");
+    }
+
+    // 3️⃣ Compare password
+    const isPasswordMatch = await bcrypt.compare(
+        payload.password,
+        user.password
+    );
+
+    if (!isPasswordMatch) {
+        throw new Error("Invalid  password");
+    }
+
+  
+    const { password, ...safeUser } = user;
+
+    // 6️⃣ Return response
+    return {
+        user: safeUser,
+      
+    };
+};
 
 export const AuthService = {
-    login
-}
+    login,
+};
--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -7,6 +7,12 @@ export default {
     node_env: process.env.NODE_ENV,
     port: process.env.PORT,
     database_url: process.env.DATABASE_URL,
-    Salt_rounds:process.env.SALT_ROUNDS
+    Salt_rounds: process.env.SALT_ROUNDS,
+    JWT: {
+        JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+        JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+        JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN,
+        JWT_REFRESH_EXPIRES_INF: process.env.JWT_REFRESH_EXPIRES_IN,
+    }
 
 }
