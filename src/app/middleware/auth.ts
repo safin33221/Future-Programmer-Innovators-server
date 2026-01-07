48b4bdee8d38ed4d37bef0d4c85a4cf120d8633d
safin33221 <safin33221@gmail.com>
2026-01-07 22:09:05 +0600

    update access token get form cookies

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -17,7 +17,7 @@ export const auth =
     (...requiredRoles: string[]) =>
         async (req: AuthRequest, res: Response, next: NextFunction) => {
             try {
-                const token = req.headers.authorization?.split(" ")[1];
+                const token = req.cookies.accessToken
 
                 if (!token) {
                     return res.status(401).json({
